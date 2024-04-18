0de6ec26a72f413175a3569baf94e4b55d303b4d
Muhammad Abdulloh <clevercoderr@gmail.com>
2024-04-18 11:39:58 +0500

    create fuse start template and add academy

---
+++ src/app/modules/admin/academy/academy.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-academy',
+  templateUrl: './academy.component.html',
+  styleUrls: ['./academy.component.scss'],
+  encapsulation  : ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone     : true,
+  imports        : [RouterOutlet],
+})
+export class AcademyComponent {
+constructor(){
+}
+}
